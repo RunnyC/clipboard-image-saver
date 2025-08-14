86daa0ae27e4b0471b809f632739035d4bd76cb1
RunnyC <21232412+RunnyC@users.noreply.github.com>
2025-08-14 18:51:13 +0800

    chore: update Homebrew formula for v1.0.2
    - Update URL to point to v1.0.2 release
    - Add correct SHA256 checksum: 8366b3c09f03dc3c0cf75293dfd28e56ba9785b43b4f5f352cb0f613cfbbdb03
    - Update test to use --help option

--- HomebrewFormula/pasteimage.rb
+++ HomebrewFormula/pasteimage.rb
@@ -1,8 +1,8 @@
 class Pasteimage < Formula
   desc "Fast clipboard image saving for macOS"
   homepage "https://github.com/RunnyC/clipboard-image-saver"
-  url "https://github.com/RunnyC/clipboard-image-saver/releases/download/v1.0.0/pasteimage-macos.tar.gz"
-  sha256 "[UPDATE_AFTER_RELEASE]"
+  url "https://github.com/RunnyC/clipboard-image-saver/releases/download/v1.0.2/pasteimage-macos.tar.gz"
+  sha256 "8366b3c09f03dc3c0cf75293dfd28e56ba9785b43b4f5f352cb0f613cfbbdb03"
   license "MIT"
 
   depends_on :macos
